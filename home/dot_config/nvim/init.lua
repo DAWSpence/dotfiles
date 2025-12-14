63caf45c3cc78dcab777b562d931002566dacb1c
DAWSpence <dawspencer@protonmail.com>
2025-12-14 17:36:40 -0500

    Update .config/nvim/init.lua Remove .config/nvim/lazy-lock.json

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -1,2 +1,23 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+require 'globals/options'
+require 'globals/keymaps'
+require 'globals/autocommands'
+
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  }
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Plugin management
+require('lazy').setup {
+  require 'lazy-plugins',
+}
+
+-- vim:tabstop=2 shiftwidth=2 expandtab syntax=lua foldmethod=marker foldlevelstart=0 foldlevel=0
