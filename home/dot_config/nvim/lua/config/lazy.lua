d85ef4810228149ed4ca6930227b1d56258cb8eb
DAWSpence <dawspencer@protonmail.com>
2026-01-02 14:30:02 -0500

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/config/lazy.lua

--- home/dot_config/nvim/lua/config/lazy.lua
+++ home/dot_config/nvim/lua/config/lazy.lua
@@ -26,6 +26,7 @@ require("lazy").setup({
     -- syntax is { import = "lazyvim.plugins.extras.<category>.<pluginname>" }
     { import = "lazyvim.plugins.extras.coding.luasnip" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
+    { import = "lazyvim.plugins.extras.coding.mini-comment" },
     { import = "lazyvim.plugins.extras.editor.mini-move" },
 
     --languages
